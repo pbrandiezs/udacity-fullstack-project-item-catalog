25a8432d83678fe9607dc82e6586a567558712e6
Perry Brandiezs <pbrandiezs@gmail.com>
2019-05-02 11:36:54 -0700

    feat: Add category table entries to create_planes.py

--- vagrant/catalog/create_planes.py
+++ vagrant/catalog/create_planes.py
@@ -44,9 +44,70 @@ except:
     print "Not added - User %s" % NewUser.username
     session.rollback()
 
+# Add Categories
+NewCategory = Category(
+    id = 1,
+    category_name = "Gulfstream"
+)
+try:
+    session.add(NewCategory)
+    session.commit()
+    print "Category %s added" % NewCategory.category_name
+except:
+    print "Not added - Category %s" % NewCategory.category_name
+    session.rollback
+
+NewCategory = Category(
+    id = 2,
+    category_name = "Crop Duster"
+)
+try:
+    session.add(NewCategory)
+    session.commit()
+    print "Category %s added" % NewCategory.category_name
+except:
+    print "Not added - Category %s" % NewCategory.category_name
+    session.rollback
+
+NewCategory = Category(
+    id = 3,
+    category_name = "Falcon"
+)
+try:
+    session.add(NewCategory)
+    session.commit()
+    print "Category %s added" % NewCategory.category_name
+except:
+    print "Not added - Category %s" % NewCategory.category_name
+    session.rollback
+
+NewCategory = Category(
+    id = 4,
+    category_name = "Embraer"
+)
+try:
+    session.add(NewCategory)
+    session.commit()
+    print "Category %s added" % NewCategory.category_name
+except:
+    print "Not added - Category %s" % NewCategory.category_name
+    session.rollback
+
+NewCategory = Category(
+    id = 5,
+    category_name = "Aerospatiale/BAC"
+)
+try:
+    session.add(NewCategory)
+    session.commit()
+    print "Category %s added" % NewCategory.category_name
+except:
+    print "Not added - Category %s" % NewCategory.category_name
+    session.rollback
+
 # Add planes
 NewPlane = ItemCatalog(
-    category_name="Gulfstream",
+    category_id=1,
     item_name="G650ER",
     item_description="Long range private jet",
     user_id=1
@@ -60,7 +121,7 @@ except:
     session.rollback()
 
 NewPlane = ItemCatalog(
-    category_name="Gulfstream",
+    category_id=1,
     item_name="G550",
     item_description="A very nice jet",
     user_id=1
@@ -74,7 +135,7 @@ except:
     session.rollback()
 
 NewPlane = ItemCatalog(
-    category_name="Gulfstream",
+    category_id=1,
     item_name="G650ER",
     item_description="Mickey Mouse's Long range private jet",
     user_id=2
@@ -88,7 +149,7 @@ except:
     session.rollback()
 
 NewPlane = ItemCatalog(
-    category_name="Crop Duster",
+    category_id=2,
     item_name="Piper Cub",
     item_description="Little yellow airplane that spews toxic chemicals on the food supply to kill beneficial insects.",
     user_id=1
@@ -102,7 +163,7 @@ except:
     session.rollback()
 
 NewPlane = ItemCatalog(
-    category_name="Falcon",
+    category_id=3,
     item_name="10X",
     item_description="The Dassault Mystere/Falcon 10 is an early corporate jet aircraft developed by French aircraft manufacturer Dassault Aviation.",
     user_id=1
@@ -116,7 +177,7 @@ except:
     session.rollback()
 
 NewPlane = ItemCatalog(
-    category_name="Embraer",
+    category_id=4,
     item_name="Phenom 100",
     item_description="The Embraer EMB-500 Phenom 100 is a very light jet developed by Brazilian aircraft manufacturer Embraer.",
     user_id=1
@@ -130,7 +191,7 @@ except:
     session.rollback()
 
 NewPlane = ItemCatalog(
-    category_name="Aerospatiale/BAC",
+    category_id=5,
     item_name="Concorde",
     item_description="The Aerospatiale/BAC Concorde is a French-British turbojet-powered supersonic passenger airliner that was operated from 1976 until 2003. It had a maximum speed over twice the speed of sound at Mach 2.04 (1,354 mph or 2,180 km/h at cruise altitude), with seating for 92 to 128 passengers.",
     user_id=1
